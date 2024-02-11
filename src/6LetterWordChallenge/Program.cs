25086835252ed4bb44c3814f26b1fc64627379cb
GencayTuran <gencay.turan@hotmail.com>
2024-02-11 23:05:10 +0100

    implemented basic data  retrieval to main

--- src/6LetterWordChallenge/Program.cs
+++ src/6LetterWordChallenge/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//get input data
+string baseDirectoryPath = AppDomain.CurrentDomain.BaseDirectory;
+string fileName = "input.txt";
+
+//Go up to the root level of the github directory to get the input file.
+string filePath = Path.GetFullPath(Path.Combine(baseDirectoryPath, "..\\..\\..\\..\\..", fileName));
+
+//put data into collection
+var inputData = await File.ReadAllLinesAsync(filePath);
+
+//return collection
+foreach (var data in inputData)
+{
+    Console.WriteLine(data);
+}
+
+Console.ReadLine();
