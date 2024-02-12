1f5c8499dfa8ad249a08e12d1907cbf202e9264e
GencayTuran <gencay.turan@hotmail.com>
2024-02-12 02:08:27 +0100

    added combination data implementation

--- src/6LetterWordChallenge/Program.cs
+++ src/6LetterWordChallenge/Program.cs
@@ -1,4 +1,5 @@
-﻿//get input data
+﻿#region InputData
+//get input data
 string baseDirectoryPath = AppDomain.CurrentDomain.BaseDirectory;
 string fileName = "input.txt";
 
@@ -7,11 +8,31 @@ string filePath = Path.GetFullPath(Path.Combine(baseDirectoryPath, "..\\..\\..\\
 
 //put data into collection
 var inputData = await File.ReadAllLinesAsync(filePath);
+#endregion
 
+//combine the data
+var combinedData = CombineData(inputData);
+
+#region ViewData
 //return collection
-foreach (var data in inputData)
+foreach (var data in combinedData)
 {
     Console.WriteLine(data);
 }
-
 Console.ReadLine();
+#endregion
+
+IEnumerable<string> CombineData(string[] inputData)
+{
+    var existingCombinations = inputData.Where(data => data.Length == 6).ToArray();
+    var toBeCombinedWords = inputData.Where(data => data.Length < 6 && data.Length > 0).ToArray();
+
+    var combinedWords = from word1 in toBeCombinedWords
+                        from word2 in toBeCombinedWords
+                        where word1 != word2
+                        let combined = word1 + word2
+                        where existingCombinations.Contains(combined, StringComparer.OrdinalIgnoreCase)
+                        select $"{word1}+{word2}={combined}";
+
+    return combinedWords;
+}
